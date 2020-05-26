fcb0ed6181731c1fa8532e47178a382876681a86
Levi Durfee <levi.durfee@gmail.com>
2020-05-26 18:38:56 -0400

    add cli

--- primes/m/main.go
+++ primes/m/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"math/big"
+	"os"
+	"strconv"
 )
 
 var knowns = []uint64{
 	82589933,
 }
 
-func mersenne(p int64) {
+func mersenne(n int64) {
 	b, y, m := &big.Int{}, &big.Int{}, &big.Int{}
 
-	y.SetInt64(p)
+	y.SetInt64(n)
 	m.SetInt64(0)
 	b.SetInt64(2)
 
@@ -23,11 +25,25 @@ func mersenne(p int64) {
 	b.Sub(b, x)
 
 	if b.ProbablyPrime(16) {
-		fmt.Printf("p: %d\n\n", p)
+		fmt.Printf("\n================\n2^n-1 | n=%d\n================\n", n)
 		fmt.Println(b.String())
 	}
 }
 
 func main() {
-	mersenne(1279)
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) < 1 {
+		fmt.Println("please provide a starting number")
+		return
+	}
+
+	s, err := strconv.Atoi(argsWithoutProg[0])
+	if err != nil {
+		panic(err)
+	}
+	x := int64(s)
+
+	for i := x; i < x*10; i++ {
+		mersenne(i)
+	}
 }
